82b1f8b58e18e3c2bf6514d433bf413715b13fa7
Robert Cox <rcox@dontbebroke.com>
2016-10-04 09:21:03 -0700

    Campaign Entity Created

---
+++ src/AppBundle/Entity/Campaigns.php
@@ -0,0 +1,165 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
+/**
+ * Campaigns
+ *
+ * @ORM\Table(name="campaigns")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\CampaignsRepository")
+ */
+class Campaigns
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     * @Assert\NotBlank()
+     */
+    private $name;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="spot_type_id", type="integer")
+     * @Assert\NotBlank()
+     */
+    private $spotTypeId;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="created_at", type="datetime")
+     * @Assert\NotBlank()
+     */
+    private $createdAt;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="updated_at", type="datetime")
+     * @Assert\NotBlank()
+     */
+    private $updatedAt;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Campaigns
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set spotTypeId
+     *
+     * @param integer $spotTypeId
+     *
+     * @return Campaigns
+     */
+    public function setSpotTypeId($spotTypeId)
+    {
+        $this->spotTypeId = $spotTypeId;
+
+        return $this;
+    }
+
+    /**
+     * Get spotTypeId
+     *
+     * @return int
+     */
+    public function getSpotTypeId()
+    {
+        return $this->spotTypeId;
+    }
+
+    /**
+     * Set createdAt
+     *
+     * @param \DateTime $createdAt
+     *
+     * @return Campaigns
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * Get createdAt
+     *
+     * @return \DateTime
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * Set updatedAt
+     *
+     * @param \DateTime $updatedAt
+     *
+     * @return Campaigns
+     */
+    public function setUpdatedAt($updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    /**
+     * Get updatedAt
+     *
+     * @return \DateTime
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+}
+
