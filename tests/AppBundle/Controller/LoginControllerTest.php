ec0f44f0064367db7949167a909340b8e5a6b287
Robert Cox <rcox@dontbebroke.com>
2016-11-10 09:46:46 -0800

    Updated Dependencies To Remove Deprication

--- tests/AppBundle/Controller/LoginControllerTest.php
+++ tests/AppBundle/Controller/LoginControllerTest.php
@@ -4,15 +4,15 @@ namespace Tests\AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-class DefaultControllerTest extends WebTestCase
+class LoginControllerTest extends WebTestCase
 {
-    public function testIndex()
+    public function testIndexAction()
     {
         $client = static::createClient();
 
-        $crawler = $client->request('GET', '/');
+        $crawler = $client->request('GET', '/login');
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
+        $this->assertContains('Login', $crawler->filter('h1.form-title')->text());
     }
 }
