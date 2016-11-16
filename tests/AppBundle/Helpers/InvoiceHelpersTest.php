f22f14bea07509b3ce8541613734cf6804db5052
Robert Cox <rcox@dontbebroke.com>
2016-11-16 14:34:50 -0800

    Testing Stuff

---
+++ tests/AppBundle/Helpers/InvoiceHelpersTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tests\AppBundle\Helpers;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class InvoiceHelpersTest extends WebTestCase
+{
+    /**
+     * Test to open file and get contents
+     */
+    public function testShowRawFile()
+    {
+    }
+}
