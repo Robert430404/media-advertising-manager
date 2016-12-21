7a2b06f2ed11adc6ba7121c6ad5a2aa37429b8f9
Robert Cox <rcox@dontbebroke.com>
2016-12-21 12:11:25 -0800

    Testing

--- web/app.php
+++ web/app.php
@@ -42,9 +42,9 @@ use Symfony\Component\Debug\Debug;
  * @var Composer\Autoload\ClassLoader $loader
  */
 $loader = require __DIR__.'/../app/autoload.php';
-Debug::enable();
+// Debug::enable();
 
-$kernel = new AppKernel('dev', true);
+$kernel = new AppKernel('prod', false);
 $kernel->loadClassCache();
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
