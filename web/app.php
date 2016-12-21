9f6ed61ee7c72dbd222fbe1b770178fc68c42f08
Robert Cox <rcox@dontbebroke.com>
2016-12-21 12:26:02 -0800

    Prod Test

--- web/app.php
+++ web/app.php
@@ -3,51 +3,30 @@
 use Symfony\Component\HttpFoundation\Request;
 
 /**
- * @var Composer\Autoload\ClassLoader
+ * This is the composer autoloader, it allows you to use composer
+ * and have all of the libraries/files be used dynamically.
+ *
+ * @var \Composer\Autoload\ClassLoader $loader
  */
 $loader = require __DIR__.'/../app/autoload.php';
+
+/**
+ * Import the PHP Symfony Cache to store views and requests.
+ */
 include_once __DIR__.'/../var/bootstrap.php.cache';
 
+/**
+ * Activate the application kernel and disable debug inside of the
+ * kernel/activate the production mode.
+ */
 $kernel = new AppKernel('prod', false);
-//$kernel->loadClassCache();
-//$kernel = new AppCache($kernel);
+$kernel->loadClassCache();
 
-// When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
-//Request::enableHttpMethodParameterOverride();
-$request = Request::createFromGlobals();
+/**
+ * Activates the symfony request object for your interactions with
+ * the application.
+ */
+$request  = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
 $kernel->terminate($request, $response);
-
-
-//use Symfony\Component\HttpFoundation\Request;
-//use Symfony\Component\Debug\Debug;
-
-// If you don't want to setup permissions the proper way, just uncomment the following PHP line
-// read http://symfony.com/doc/current/book/installation.html#checking-symfony-application-configuration-and-setup
-// for more information
-//umask(0000);
-
-// This check prevents access to debug front controllers that are deployed by accident to production servers.
-// Feel free to remove this, extend it, or make something more sophisticated.
-//if (isset($_SERVER['HTTP_CLIENT_IP'])
-//    || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
-//    || !(in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1']) || php_sapi_name() === 'cli-server')
-//) {
-//    header('HTTP/1.0 403 Forbidden');
-//    exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
-//}
-
-///**
-// * @var Composer\Autoload\ClassLoader $loader
-// */
-//$loader = require __DIR__.'/../app/autoload.php';
-//// Debug::enable();
-//
-//$kernel = new AppKernel('dev', true);
-//$kernel->loadClassCache();
-//
-//$request = Request::createFromGlobals();
-//$response = $kernel->handle($request);
-//$response->send();
-//$kernel->terminate($request, $response);
